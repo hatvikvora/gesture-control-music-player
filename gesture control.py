02d5ecdd756c4dd529c7d9d2a4dd6d191ab0ef41
hatvikvora <hatvikvora@gmail.com>
2021-04-06 18:15:05 +0530

    added pygame to control audio

--- gesture control.py
+++ gesture control.py
@@ -1,6 +1,4 @@
-import os
-import subprocess
-import glob
+from pygame import mixer
 import RPi.GPIO as GPIO
 import time
 from time import sleep
@@ -10,15 +8,15 @@ ECHO = 24
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(TRIGGER,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
-os.chdir('/home/pi/Music')
-musicFilePath ='Preet.mp3'
-f = glob.glob('Preet.mp3')
-h = len(f)
-status = 1
-pointer = 0
-start = 0
-volume = 20
-    
+mixer.init()
+mixer.music.load("/home/pi/Music/Preet.mp3")
+
+def vol(float):
+    mixer.music.set_volume(float)
+
+vol(0.7)
+mixer.music.play()
+
 def distance():
     GPIO.output(TRIGGER, GPIO.LOW)
     print ("Waiting for sensor to settle")
@@ -34,35 +32,28 @@ def distance():
         pulse_duration = pulse_end_time - pulse_start_time
     distance = round(pulse_duration * 17150, 2)
     print ("Distance:",distance,"cm")
-    return distance
+    return d
       
-for d in range(4,4000):
+for d in range(4,1000):
     d= int (distance())
-    player = subprocess.Popen(['omxplayer', '-b', musicFilePath],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=True)
-    fi = player.poll()
     while d==True:
-        player.stdin.write("p")
-        if d>4 and d<=45:        
-            vol = d/3
-            if(vol>volume):
-                for counter in range(1,vol-volume):
-                    player.stdin.write("+")
-                    volume = volume + 1
-                    sleep(0.1)
-            elif(vol<volume):
-                for counter in range(1,volume-vol):
-                    player.stdin.write("-")
-                    volume = volume - 1
-                    sleep(0.1)
-                    print("play while 4-45cm")
+        vol(0.7)
+        mixer.music.play()
+        if d>4 and d<=15:        
+            vol(0.3)
+            print("volune 3 while 4-15cm")
+        elif d>15 and d<=30:
+            vol(1)
+            print("volune 10 while 15-30cm")
+        elif d>30 and d<=45:
+            vol(1.5)
+            print("volune 15 while 30-45cm")
         elif d>45:
-            volume=0
+            vol(None)
             print("mute")
-        else:
-            player.stdin.write("q")
-            print("out of range")
+        
 
 while KeyboardInterrupt:
+    mixer.music.pause()
     print("Stopped by user")
     GPIO.cleanup()
-    
