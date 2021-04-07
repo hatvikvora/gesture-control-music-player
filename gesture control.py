8dfc9cb855d30e448e8a75697c6d8bfc2fbcc4cd
hatvikvora <hatvikvora@gmail.com>
2021-04-07 14:36:11 +0530

    changed loop condition and added other play pause feature

--- gesture control.py
+++ gesture control.py
@@ -1,7 +1,8 @@
-from pygame import mixer
-import RPi.GPIO as GPIO
 import time
 from time import sleep
+
+import RPi.GPIO as GPIO
+from pygame import mixer
 
 TRIGGER = 23
 ECHO = 24
@@ -32,28 +33,25 @@ def distance():
         pulse_duration = pulse_end_time - pulse_start_time
     distance = round(pulse_duration * 17150, 2)
     print ("Distance:",distance,"cm")
-    return d
-      
-for d in range(4,1000):
-    d= int (distance())
-    while d==True:
-        vol(0.7)
-        mixer.music.play()
-        if d>4 and d<=15:        
-            vol(0.3)
-            print("volune 3 while 4-15cm")
-        elif d>15 and d<=30:
-            vol(1)
-            print("volune 10 while 15-30cm")
-        elif d>30 and d<=45:
-            vol(1.5)
-            print("volune 15 while 30-45cm")
-        elif d>45:
-            vol(None)
-            print("mute")
-        
+    return distance
 
-while KeyboardInterrupt:
+while True :
+    d = int(distance())
+    if d>4 and d<=15:
+        vol(0.3)
+        print("volune 3 while 4-15cm")
+    elif d>15 and d<=30:
+        vol(1)
+        print("volune 10 while 15-30cm")
+    elif d>30 and d<=45:
+        vol(1.5)
+        print("volune 15 while 30-45cm")
+    if d<45:
+        mixer.music.unpause()
+    if d < 4 or d > 45:
+        mixer.music.pause()
+    
+if KeyboardInterrupt :
     mixer.music.pause()
     print("Stopped by user")
     GPIO.cleanup()
